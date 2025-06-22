5aac08df96a8b5c073c769596e5df362d6bf2d8d
Armin Burgmeier <armin@cradle.bio>
2025-06-23 11:41:03 +1200

    Don't (ab)use json for metadata dump

--- custom_predictor/entrypoints/metadata.py
+++ custom_predictor/entrypoints/metadata.py
@@ -1,20 +1,35 @@
 import dataclasses
-import json
-import sys
+from typing import Any
 
 from custom_predictor import METADATA
 
 
-def _json_normalize(x):
-    if dataclasses.is_dataclass(x):
-        return dataclasses.asdict(x)  # pyright: ignore[reportArgumentType]
-    if isinstance(x, type):
-        return x.__name__
-    raise TypeError(x)
+def render(obj: Any) -> str:
+    if isinstance(obj, type):
+        return obj.__name__
+    return "{}".format(obj)
+
+
+def dump_object(obj: Any, indent: str = ""):
+    if dataclasses.is_dataclass(obj):
+        for field in dataclasses.fields(obj):
+            value = getattr(obj, field.name)
+            field_display_name = field.name.replace("_", " ").title()
+
+            if isinstance(value, (tuple, list)):
+                print("{}{}:".format(indent, field_display_name))
+                for i, elem in enumerate(value):
+                    if i > 0 and dataclasses.is_dataclass(elem):
+                        print("{}---".format(indent + "  "))
+                    dump_object(elem, indent=indent + "  ")
+            else:
+                print("{}{}: {}".format(indent, field_display_name, render(value)))
+    else:
+        print("{}{}".format(indent, render(obj)))
 
 
 def dump_metadata():
-    json.dump(METADATA, sys.stdout, indent=4, default=_json_normalize)
+    dump_object(METADATA)
 
 
 if __name__ == "__main__":
